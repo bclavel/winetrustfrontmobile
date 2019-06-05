b55b0f95b4962f1b02302f1884abdc4420631fc0
clemauffgith <clement.auffray@gmail.com>
2019-06-05 19:10:46 +0200

    dynamisation screen produit mobile

--- Components/Screens/ProductScreen.js
+++ Components/Screens/ProductScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Platform, StyleSheet, Text, View, Image, WebView, ScrollView } from 'react-native';
+import { Platform, StyleSheet, Text, View, Image, WebView, ScrollView, Linking } from 'react-native';
 import { Divider, Button } from 'react-native-elements';
 import Transaction from './Transaction';
 
@@ -16,28 +16,28 @@ class ProductScreen extends React.Component {
       render() {
         return (
           <ScrollView style={{flex : 1 }}>
-            <Image style= {styles.image} source={require('../../assets/beauregard4.jpg')}/>
-            <Text style={styles.h1}>{this.props.product.productDomaine}</Text>
-            <Text style={styles.h4}>Domaine Beauregard</Text>
-            <Text style={styles.h4}>Pomerol AOC</Text>
-            <Text style={styles.h4}>Région de Bordeaux, France</Text>
+            <Image style= {styles.image} source= {{uri: this.props.product.productMobImg}}/>
+            <Text style={styles.h1}>{this.props.product.productCuvee + ' ' + this.props.product.productMillesime}</Text>
+            <Text style={styles.h4}>{this.props.product.productDomaine}</Text>
+            <Text style={styles.h4}>{this.props.product.productAppellation}</Text>
+            <Text style={styles.h4}>{this.props.product.productRegion + ', ' + this.props.product.productCountry}</Text>
             <Button title='Acheter 49,90€' buttonStyle={{ marginTop: 20 }} color= "white" backgroundColor= "#22323F"/>
             <Text style={styles.smallTxt}>Vendu par Ovinia.com</Text>
             <Divider style={{ backgroundColor: 'black', marginTop : 15, marginBottom : 15 }} />
-            <Text style={styles.h2}>Découvrez le domaine Beauregard</Text>
-            <Text style={styles.standardTxt}>www.chateau-beauregard.com</Text>
+            <Text style={styles.h2}>{'Découvrez le ' + this.props.product.productDomaine }</Text>
+            <Text style={styles.standardTxt} onPress={()=> {Linking.openURL("http://"+this.props.product.productdomainUrl)}}>{this.props.product.domainUrl}</Text>
             <View style={{flex: 1, flexDirection:'row', alignItems : 'center'}}>
               <Image style={styles.imagePicto} source={require('../../assets/picto_email.png')}/>
-              <Text style={styles.standardTxt}>Par email</Text>
+              <Text style={styles.standardTxt}>{this.props.product.domainEmail}</Text>
             </View>
             <View style={{flex: 1, flexDirection:'row', alignItems : 'center'}}>
               <Image style={styles.imagePicto} source={require('../../assets/picto_facebook.png')}/>
-              <Text style={styles.standardTxt}>Page Facebook</Text>
+              <Text style={styles.standardTxt}>{this.props.product.domainFacebook}</Text>
             </View>
             <Divider style={{ backgroundColor: 'black', marginTop : 15, marginBottom : 15 }} />
             <Text style={styles.h2}>Caractéristiques</Text>
-            <Text style={styles.standardTxt}><Text style={{fontWeight: 'bold'}}>Cépages</Text> : 95% Pinot noir, 5% Merlot</Text>
-            <Text style={styles.standardTxt}><Text style={{fontWeight: 'bold'}}>Accord mets & vin</Text> : Viande rouge, Gibier</Text>
+            <Text style={styles.standardTxt}><Text style={{fontWeight: 'bold'}}>Cépages : </Text>{this.props.product.productCepages}</Text>
+            <Text style={styles.standardTxt}><Text style={{fontWeight: 'bold'}}>Accord mets & vin : </Text>{this.props.product.productAccords}</Text>
             <Divider style={{ backgroundColor: 'black', marginTop : 15, marginBottom : 15 }} />
             <Text style={styles.h2}>Histoire du domaine</Text>
             <View style={{ height: 300 }}>
@@ -45,11 +45,10 @@ class ProductScreen extends React.Component {
                      style={ styles.WebViewContainer }
                      javaScriptEnabled={true}
                      domStorageEnabled={true}
-                     source={{uri: 'https://www.youtube.com/embed/jGPVRxth6Dk' }}
+                     source= {{uri: this.props.product.productYoutube.replace('watch?v=', 'embed/') }}
              />
             </View>
-            <Text style={styles.standardTxt}><Text style={{fontStyle : 'italic'}}>Propriété des familles Moulin et Cathiard depuis 2014, le Château Beauregard renaît autour de sa fonction première : produire un grand vin de Pomerol au cœur d’un lieu historiquement dédié à la résidence de ses hôtes.</Text>
-            L’histoire de Beauregard remonte au XIIe siècle et aux Chevaliers Hospitaliers de Saint Jean de Jérusalem, à qui l’on doit la célèbre Croix des Templiers, emblème de Beauregard. Actifs dans la région de Pomerol et propriétaires d’un petit manoir ils cultivaient déjà ces terres. Sur ce site, la famille Beausoleil fait construire cinq siècles plus tard un premier édifice, qui fut remplacé à l’époque napoléonienne par le Château actuel : une magnifique chartreuse girondine, qui s’ouvre sur une terrasse avec deux pigeonniers surplombant des douves et un très beau parc, œuvre d’un élève de Victor Louis, architecte du Grand Théâtre de Bordeaux.</Text>
+            <Text style={styles.standardTxt}></Text><Text style={{fontStyle : 'italic'}}>{this.props.product.domainHistory}</Text>
             <Divider style={{ backgroundColor: 'black', marginTop : 15, marginBottom : 15 }} />
             <Text style={styles.h2}>Historique des transactions</Text>
             <Transaction />
@@ -110,6 +109,7 @@ class ProductScreen extends React.Component {
         marginLeft : 5,
       },
       image: {
-        width: '100%'
+        width: '100%',
+        height: 160
       }
     });
